202ea324a4efc739effaf3e28bc3827ce3e38d0d
abangura2 <abangura2@acamdemic.rrc.ca>
2024-04-12 09:54:39 -0500

    Initial commit: Set up project directory and file structure

---
+++ tests/test_mortgage.py
@@ -0,0 +1,7 @@
+"""
+Description: A class used to test the Mortgage class.
+Author: AbdulRahman Hassan Bangura
+Date: 08/04/24
+Usage: Use the tests encapsulated within this class to test the MortgagePayment class.
+"""
+
