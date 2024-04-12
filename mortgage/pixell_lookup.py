73923cfa9cf4e3c3f25a40dae168ccfac33978fa
abangura2 <abangura2@acamdemic.rrc.ca>
2024-04-12 10:03:01 -0500

    Initial commit: Set up project directory and file structure

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -2,8 +2,8 @@
 Description: Enumerations to keep track of valid mortgage rates 
 and payment frequencies. A list to keep track of valid amortization periods.
 Author: ACE Department
-Edited By: {Student Name}
-Date: {Date}
+Edited By: AbdulRahman Hassan Bangura
+Date: 08/04/2024
 Usage: The enumerations and list in this file may be used when working 
 with mortgages to ensure only valid rates, frequencies and amortization 
 periods are used.
