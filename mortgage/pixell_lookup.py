4e4007c03892460ece5e40703671274bdf450a75
abangura2 <abangura2@acamdemic.rrc.ca>
2024-04-12 10:46:51 -0500

    Define Accessors and Mutators for Loan Amount

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -9,8 +9,22 @@ with mortgages to ensure only valid rates, frequencies and amortization
 periods are used.
 """
 
-
+# Importing Enum class from the enum module.
 from enum import Enum
 
-VALID_AMORTIZATION = {5, 10, 15, 20, 25, 30}
+class MortgageRate(Enum):
+    """An enumeration representing valid mortgage rates."""
+    FIXED_5 = 0.0519
+    FIXED_3 = 0.0589
+    FIXED_1 = 0.0599
+    VARIABLE_5 = 0.0649
+    VARIABLE_3 = 0.0669
+    VARIABLE_1 = 0.0679
 
+class PaymentFrequency(Enum):
+    """An enumeration representing valid payment frequencies."""
+    MONTHLY = 12
+    BI_WEEKLY = 26
+    WEEKLY = 52
+
+VALID_AMORTIZATION = {5, 10, 15, 20, 25, 30}
